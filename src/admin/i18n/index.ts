9ffd5d8113abd38c5de3b49eba1a819c7842e916
Viorel-Cosmin Miron <cosmin@uhl-services.ch>
2026-06-01 20:30:31 +0200

    chore(i18n): align admin locales to de-CH and en-US

---
+++ src/admin/i18n/index.ts
@@ -0,0 +1,7 @@
+import enUS from "./json/en-US.json" with { type: "json" }
+import deCH from "./json/de-CH.json" with { type: "json" }
+
+export default {
+  "en-US": { translation: enUS },
+  "de-CH": { translation: deCH },
+}
