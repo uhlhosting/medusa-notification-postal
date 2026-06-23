70f4dfc7f481dba4a9e82ceb1280abdea24ea6db
Viorel-Cosmin Miron <cosmin@uhl-services.ch>
2026-06-23 04:40:01 +0200

    chore: prepare 0.1.5 release

--- src/admin/i18n/index.ts
+++ src/admin/i18n/index.ts
@@ -3,10 +3,5 @@ import de from "./json/de.json" with { type: "json" }
 
 export default {
   en: { translation: en },
-  "en-US": { translation: en },
-  "en-GB": { translation: en },
   de: { translation: de },
-  "de-CH": { translation: de },
-  fr: { translation: en },
-  cs: { translation: en },
 }
